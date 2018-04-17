477eb8a94c4bd9613fd2f561275380d55ccf581c
Administratör <Administratr@WIN-TQTMJ4UUBF9>
2018-04-17 15:07:03 +0200

    Fixat CRD av CRUD. Bra jobbat, bois. :-)

--- GruppFem/Controllers/HomeController.cs
+++ GruppFem/Controllers/HomeController.cs
@@ -3,15 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GruppFem.ServiceReference1;
 
 namespace GruppFem.Controllers
 {
     public class HomeController : Controller
     {
+
+        ServiceReference1.Service1Client client = new Service1Client();
+
         // GET: Home
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Users()
+        {
+
+            return View(client.GetUserInfo().ToList());
+        }
+
+        public ActionResult CreateEstablishment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEstablishment(string name, string description)
+        {
+            client.CreateEstablishment(name, description);
+
+            return View("Index");
+        }
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(string username, string password, string firstname, string lastname, string email)
+        {
+            client.CreateUser(username, password, firstname, lastname, email);
+            return View("Index");
+        }
+
+        public ActionResult Establishments()
+        {
+
+            return View(client.GetEstablishmentInfo().ToList());
+        }
+        [HttpPost]
+        public ActionResult Establishments(int establishmentID)
+        {
+
+            return View(client.GetEstablishmentInfo().ToList());
+        }
+
+        public ActionResult DeleteUser(int userID)
+        {
+            client.DeleteUser(userID);
+
+            return View("Index");
+        }
+
+        public ActionResult DeleteEstablishment(int establishmentID)
+        {
+            client.DeleteEstablishment(establishmentID);
+
+            return View("Index");
         }
     }
 }
